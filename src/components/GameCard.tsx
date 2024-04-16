16cfc8f73a92d60ba9ba39a04f4cc74634439d30
Yeduin <ymateo@objectlink.net>
2024-04-16 15:56:22 -0400

    Displaying platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Game, Platform } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 interface Props {
   game: Game;
 }
@@ -13,6 +14,9 @@ const GameCard = ({ game }: Props) => {
           <Heading fontSize="1xl" size="md">
             {game.name}
           </Heading>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
         </CardBody>
       </Card>
     </>
