70eb45e1f7ec606d45dd6c86562c5b7b674e4d76
yeduin <el_yeduin_02@hotmail.com>
2024-04-15 21:40:55 -0400

    addind templateAreas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <Button colorScheme="blue">Button</Button>{" "}
-    </div>
+    <>
+      <Grid
+        templateAreas={{
+          base: ` "nav" "main" "aside"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bgColor="coral">
+          nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bgColor="gold">
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bgColor="dodgerblue">
+          main
+        </GridItem>
+      </Grid>
+    </>
   );
 };
 
